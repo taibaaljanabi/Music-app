fae796f598265b8cd118b0ee54e6a6caab137d29
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-08-14 00:10:46 -0600

    commit

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 window.addEventListener('load' , () => {
     const sounds = document.querySelectorAll('.sound')
     const pads = document.querySelectorAll('.pads div')
-
-    console.log(sounds)
+    const visual = document.querySelector('.visual')
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "lightpink",
+        "lightsalmon",
+        "lightgreen",
+       " purple"
+    ]
     
-    pads.forEach(pad =>{
-        pad.addEventListener('click', function (){
-            sounds[4]
-        })
-    })
 
+    console.log(sounds[0])
+    
+    pads.forEach((pad, index) =>{
+        pad.addEventListener('click', function (){
+            sounds[index].currentTime = 0
+            sounds[index].play()
+            creatBubbles(index)
+        })
+       
+    })
+ const creatBubbles = (index)=>{
+     const bubble = document.createElement('div')
+     visual.appendChild(bubble)
+     bubble.style.backgroundColor= colors[index]
+     bubble.style.animation ='jump 1s ease'
+     bubble.addEventListener('animationend', function (){
+         visual.removeChild(this)
+     })
+ }
 })
